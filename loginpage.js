b476f8bc81b6ded9f0a99746fbb3b5f657677426
jeevanbr357 <jeevanv229@gmail.com>
2025-01-11 14:56:38 +0530

    Update loginpage.js

--- loginpage.js
+++ loginpage.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./LoginForm.css";
+import "./LoginForm.css"; // Linking the CSS file
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
